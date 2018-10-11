4543786bcef425dcd09360250949407cc7a1465b
galJacob <joojonogal2@gmail.com>
2018-10-12 01:00:39 +0300

    fixed  bug

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,7 +19,6 @@ class Header extends Component {
         })
     }
     setActiveLink = linkType => {
-        if (!this.state.loggedInUser) return;
         this.setState({ isActive: linkType });
     }
     render() {
